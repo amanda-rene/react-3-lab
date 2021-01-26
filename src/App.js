919c3e14c39ab89b98c4a622598b9d8bd0f6808a
Amanda Mattleman <amandamattleman@MSI>
2021-01-26 09:09:59 -0700

    displayarr

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ constructor(){
 }
 
 
+
   render(){
 
   return (
--- src/Main.js
+++ src/Main.js
@@ -9,27 +9,41 @@ constructor(){
     super();
 
     this.state = {
-        data:data
+        data:data,
+        i: 0
     };
 
     
 }
 
 
-dataList =() => {
-    for (let i = 0; i < data.length; i++){
-        console.log(this.dataList)
-    }
-}
+// dataList =() => {
+//     for (let i = 0; i < data.length; i++){
+        
+//     }
+// }
 
 render(){
 
-    const Main = (props) => {
-        console.log(props)}
+  const dataMap = this.state.data.map( data => {
+      return ( <div>{data.name.first} </div>)
+  })
 
     return(
       <div className = 'main'>
-          
+          {this.state.data[this.state.i].name.first}
+          <br></br>
+          {this.state.data[this.state.i].name.last}
+          <br></br>
+          {this.state.data[this.state.i].city}
+          <br></br>
+          {this.state.data[this.state.i].country}
+          <br></br>
+          {this.state.data[this.state.i].employer}
+          <br></br>
+          {this.state.data[this.state.i].title}
+          <br></br>
+          {this.state.data[this.state.i].favoriteMovies}
       </div>)
     
   }
